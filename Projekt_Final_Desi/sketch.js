c4b069da130fd7e36be972f4448cc9a975bfb07d
DesiLanz <lanzdesiree@gmail.com>
2020-10-29 19:16:54 +0100

    test
    test

--- Projekt_Final_Desi/sketch.js
+++ Projekt_Final_Desi/sketch.js
@@ -15,7 +15,7 @@ function setup() {
   angleMode(DEGREES);
   colorMode(HSB);
 
-  gui = createGui('p5.gui');
+  gui = createGui('Windstärke');
   gui.addGlobals('laenge');
 }
 
@@ -27,11 +27,11 @@ function draw() {
   strokeWeight(3);
 
 
-  if (mouseIsPressed){
-      blendMode(LIGHTEST);
-    }else{
-      blendMode(MULTIPLY);
-    }
+  if (mouseIsPressed) {
+    blendMode(LIGHTEST);
+  } else {
+    blendMode(MULTIPLY);
+  }
 
 
   if (up == 0) {
@@ -55,106 +55,103 @@ function draw() {
     for (let y = 1; y <= height; y += 10) {
 
       push();
-      translate(x + 30, 10 * y + 10+ laenge);
+      translate(x + 30, 10 * y + 10 + laenge);
       rotate(angle);
-      translate(-x - 30, -10 * y - 10- laenge);
+      translate(-x - 30, -10 * y - 10 - laenge);
       line(x + 30, 10 * y, x + 30, 10 * y + 10 + laenge);
       pop();
 
       push();
-      translate(x + 100, 30 * y + 30+ laenge);
+      translate(x + 100, 30 * y + 30 + laenge);
       rotate(angle);
-      translate(-x - 100, -30 * y - 30- laenge);
+      translate(-x - 100, -30 * y - 30 - laenge);
       line(x + 100, 30 * y, x + 100, 30 * y + 30 + laenge);
       pop();
 
       push();
-      translate(x + 180, 140 * y + 60+ laenge);
+      translate(x + 180, 140 * y + 60 + laenge);
       rotate(angle);
-      translate(-x - 180, -140 * y - 60- laenge);
+      translate(-x - 180, -140 * y - 60 - laenge);
       line(x + 180, 140 * y, x + 180, 140 * y + 60 + laenge);
       pop();
 
       push();
-      translate(x, 70 * y + 20+ laenge);
+      translate(x, 70 * y + 20 + laenge);
       rotate(angle);
-      translate(-x, -70 * y - 20- laenge);
+      translate(-x, -70 * y - 20 - laenge);
       line(x, 70 * y, x, 70 * y + 20 + laenge);
       pop();
 
       push();
-      translate(x + 570, 50 * y + 80+ laenge);
+      translate(x + 570, 50 * y + 80 + laenge);
       rotate(angle);
-      translate(-x - 570, -50 * y - 80- laenge);
+      translate(-x - 570, -50 * y - 80 - laenge);
       line(x + 570, 50 * y, x + 570, 50 * y + 80 + laenge);
       pop();
 
       push();
-      translate(x + 920, 10 * y + 30+ laenge);
+      translate(x + 920, 10 * y + 30 + laenge);
       rotate(angle);
-      translate(-x - 920, -10 * y - 30- laenge);
+      translate(-x - 920, -10 * y - 30 - laenge);
       line(x + 920, 10 * y, x + 920, 10 * y + 30 + laenge);
       pop();
 
       push();
-      translate(x + 720, 140 * y + 50+ laenge);
+      translate(x + 720, 140 * y + 50 + laenge);
       rotate(angle);
-      translate(-x - 720, -140 * y - 50- laenge);
+      translate(-x - 720, -140 * y - 50 - laenge);
       line(x + 720, 140 * y, x + 720, 140 * y + 50 + laenge);
       pop();
 
       push();
-      translate(x + 530, 42 * y + 10+ laenge);
+      translate(x + 530, 42 * y + 10 + laenge);
       rotate(angle);
-      translate(-x - 530, -42 * y - 10- laenge);
+      translate(-x - 530, -42 * y - 10 - laenge);
       line(x + 530, 42 * y, x + 530, 42 * y + 10 + laenge);
       pop();
 
       push();
-      translate(x + 510, 210 * y + 50+ laenge);
+      translate(x + 510, 210 * y + 50 + laenge);
       rotate(angle);
-      translate(-x - 510, -210 * y - 50- laenge);
+      translate(-x - 510, -210 * y - 50 - laenge);
       line(x + 510, 210 * y, x + 510, 210 * y + 50 + laenge);
       pop();
 
       push();
-      translate(x + 510, 510 * y + 50+ laenge);
+      translate(x + 510, 510 * y + 50 + laenge);
       rotate(angle);
-      translate(-x - 510, -510 * y - 50- laenge);
+      translate(-x - 510, -510 * y - 50 - laenge);
       line(x + 510, 510 * y, x + 510, 510 * y + 50 + laenge);
       pop();
 
       push();
-      translate(x + 450, 700 * y + 80+ laenge);
+      translate(x + 450, 700 * y + 80 + laenge);
       rotate(angle);
-      translate(-x - 450, -700 * y - 80- laenge);
+      translate(-x - 450, -700 * y - 80 - laenge);
       line(x + 450, 710 * y, x + 450, 710 * y + 70 + laenge);
       pop();
 
       push();
-      translate(x, 32 * y + 40+ laenge- laenge);
+      translate(x, 32 * y + 40 + laenge - laenge);
       rotate(angle);
       translate(-x, -32 * y - 40);
       line(x, 32 * y, x, 32 * y + 40 + laenge);
       pop();
 
       push();
-      translate(x + 850, 32 * y + 10+ laenge);
+      translate(x + 850, 32 * y + 10 + laenge);
       rotate(angle);
-      translate(-x - 850, -32 * y - 10- laenge);
+      translate(-x - 850, -32 * y - 10 - laenge);
       line(x + 850, 32 * y, x + 850, 32 * y + 10 + laenge);
       pop();
 
       push();
-      translate(x + 1200, 30 * y + 80+ laenge);
+      translate(x + 1200, 30 * y + 80 + laenge);
       rotate(angle);
-      translate(-x - 1200, -30 * y - 80- laenge);
+      translate(-x - 1200, -30 * y - 80 - laenge);
       line(x + 1200, 30 * y, x + 1200, 30 * y + 80 + laenge);
       pop();
     }
   }
-
-
-
 
 }
